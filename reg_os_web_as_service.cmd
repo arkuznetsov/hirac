2561cb76f62f6c27e2d93c547eb1f3e5472b88c8
Artem Kuznetsov <ArKuznetsov@gmail.com>
2019-10-24 19:29:43 +0300

    Доработан командный файл установки приложения в качестве службы

--- reg_os_web_as_service.cmd
+++ reg_os_web_as_service.cmd
@@ -1,10 +1,10 @@
 @echo off
-set BasePath="%2"
-set addr="http://PC063001:5005/"
+set OscriptWebBinary = "%1"
+set addr="%2"
+set ContentRoot="%3"
 set SrvcName="1C:HTTP Interface for RAC"
-set BinPath="%1 --urls=%addr% --BasePath=%BasePath%"
+set BinPath="%OscriptWebBinary% --RunAsService --urls=%addr% --ContentRoot=%ContentRoot%"
 set Desctiption="1C HTTP Interface for remote administration"
 sc stop %SrvcName%
 sc delete %SrvcName%
-rem sc create %SrvcName% binPath= %BinPath% start= auto obj= %SrvUserName% password= %SrvUserPwd% displayname= %Desctiption%
 sc create %SrvcName% binPath= %BinPath% start= auto displayname= %Desctiption%

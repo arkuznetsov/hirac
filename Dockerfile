2fc64ee1a4cdd99e15090ab6e3fcbd23cc6a5991
thedemoncat <nl.ruslan@yandex.ru>
2021-02-07 20:33:09 +0600

    исправил версии

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,15 @@ RUN set -xe \
     && apt-get install -y mono-complete mono-devel \
     && wget https://oscript.io/downloads/latest/x64/onescript-engine_${OSCRIPT_VERSION}_all.deb -O oscript.deb \
     && dpkg -i oscript.deb \
-    && rm oscript.deb 
+    && rm oscript.deb \
+    && rm -rf /var/lib/apt/lists/* 
 
 COPY src/ /app
 
 RUN cd /app \
     && opm install -l
 
-FROM evilbeaver/oscript-web:dev
+FROM evilbeaver/oscript-web:latest
 COPY --from=onec-full /opt/1C/v8.3/x86_64 /opt/1C/v8.3/x86_64
 COPY --from=onec-full /app /app
 
--- build_images.sh
+++ build_images.sh
@@ -3,12 +3,14 @@ set -e
 
 export $(grep -v '^#' .env | xargs)
 
+HIRAC_NAME_IMAGE="${1:-oscript/hirac:latest}"
+
 docker build -t demoncat/onec:full-"$ONEC_VERSION" \
     --build-arg ONEC_USERNAME="$ONEC_USERNAME" \
     --build-arg ONEC_PASSWORD="$ONEC_PASSWORD"  \
     --build-arg VERSION="$ONEC_VERSION" tools/docker/onec-full
 
 
-docker build -t demoncat/onec:full-osweb-test-"$ONEC_VERSION" \
+docker build -t "$HIRAC_NAME_IMAGE" \
     --build-arg ONEC_VERSION="$ONEC_VERSION" \
     --build-arg OSCRIPT_VERSION="$OSCRIPT_VERSION" .
